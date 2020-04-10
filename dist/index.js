c78d729618c12780bf9e9ead2e33ad257f876b11
Bart Veneman <bartveneman@users.noreply.github.com>
2020-04-10 16:30:10 +0200

    mark outdated

--- dist/index.js
+++ dist/index.js
@@ -8552,26 +8552,65 @@ async function run() {
 		// Read CSS file
 		// const css = fs.readFileSync(cssPath, 'utf8')
 
-		// Find any previous comments
-
-		// And mark them as OUTDATED
-		await octokit.graphql(
+		// Find any previous comments by GitHub Bot
+		const { data } = await octokit.graphql(
 			`
-			mutation MarkCommentOutdated($input: MinimizeCommentInput!) {
-				minimizeComment(input: $input) {
-					minimizedComment {
-						minimizedReason
+			query RecentBotComments(
+				$owner: String!,
+				$repoName: String!,
+				$prNumber: Int!
+			) {
+				repository(owner: $owner, name: $repoName) {
+					pullRequest(number: $prNumber) {
+						comments(last: 10) {
+							totalCount
+							edges {
+								node {
+									id
+									bodyText
+									author {
+										login
+									}
+								}
+							}
+						}
 					}
 				}
 			}
 			`,
 			{
-				input: {
-					classifier: 'OUTDATED',
-					subjectId: 'MDEyOklzc3VlQ29tbWVudDYxMjA0MDA5Ng==',
-				},
+				owner: payload.repository.owner.login,
+				repoName: payload.repository.name,
+				prNumber: payload.pull_request.number,
 			}
 		)
+
+		if (data.repository.pullRequest.comments.totalCount > 0) {
+			// And mark them as OUTDATED
+			await Promise.all(
+				data.repository.pullRequest.comments.edges
+					.map(({ node }) => node.id)
+					.map((id) => {
+						return octokit.graphql(
+							`
+					mutation MarkCommentOutdated($input: MinimizeCommentInput!) {
+						minimizeComment(input: $input) {
+							minimizedComment {
+								minimizedReason
+							}
+						}
+					}
+					`,
+							{
+								input: {
+									classifier: 'OUTDATED',
+									subjectId: id,
+								},
+							}
+						)
+					})
+			)
+		}
 
 		// POST the actual PR comment
 		const formattedBody = `
